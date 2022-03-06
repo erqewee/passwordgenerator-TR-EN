bd19f079f8f677283c9dada2b06c4b4f0a9eeee5
EreniXq <99599521+EreniXq@users.noreply.github.com>
2022-03-06 19:55:41 +0300

    Update passgen.py

--- passgen.py
+++ passgen.py
@@ -1,14 +1,28 @@
 from random import sample
 from string import ascii_letters, ascii_lowercase, ascii_uppercase, punctuation, digits
 from datetime import datetime
+
 All = (ascii_letters + ascii_lowercase + ascii_uppercase + punctuation + digits)
 
 userName = input("Adınızı Giriniz: ")
 
 if(userName == ""):
- userName += "None"
+ userName += "Belirtilmedi!"
  
-val = int(input("İşlem Türünü seçiniz! (Oluşturana ekle - 1 | Düz oluştur - 2 | Şifreleri Göster - 3): "))
+val = int(input("""
+=> İşlem Türünü seçiniz! 
+
+|---------------------|
+| 1 - Oluşturana ekle |
+|---------------------|
+| 2 - Düz oluştur     |
+|---------------------|
+| 3 - Şifreleri Oku   |
+|---------------------|
+| 0 - Temizle         |
+|---------------------|
+
+> İşleminiz: """))
 
 fileName = "lastCreatedPassword-log.js"
 file = open(fileName, "w", encoding = "utf-8")
@@ -16,35 +30,45 @@ file = open(fileName, "w", encoding = "utf-8")
 fileName2 = "allCreatedPassword-logs.txt"
 file2 = open(fileName2, "a", encoding = "utf-8")
 
-# İçerikleri oku
-def readCreatedPasswords(readFile):
-   with open(readFile) as i:
-    print(i.read())
-    print("Sistem uygulamayı başarılı bir şekilde çalıştırdı!")
+def ClearLogs():
+ with open(fileName, "w") and open(fileName2, "w") as i:
+  print("'%(fileOne)s' ve '%(fileTwo)s' Temizlendi!" % { "fileOne": fileName, "fileTwo": fileName2})
 
+
+def readCreatedPasswords(nameOne, nameTwo):
+  with open(nameOne) as dataFile:
+   print(dataFile.read())
+
+
+# İçerikleri oku
 if(val == 3):
- print("Sistem uygulamayı başlatmayı deniyor...")
- readCreatedPasswords(fileName2);
+ readCreatedPasswords(fileName2, fileName);
+
+elif(val == 0): 
+ ClearLogs();
+
 
 else:
+
  keyLength = int(input("Uzunluk Sayısını Giriniz: "))
 
  if(keyLength >= 100):
   print("Sistem Hatası, Max 100 uzunlukta şifre oluşturabilirsiniz.")
 
  today = datetime.today()
- time = """{ "Year": "%(year)s", "Ay": "%(month)s", "Day": "%(day)s", "Saat": "%(hour)s:%(minute)s:%(second)s" }""" % { "year": today.year, "month": today.month, "day": today.day, "hour": today.hour, "minute": today.minute, "second": today.second } 
+ time = """{ "Year": "%(year)s", "Month": "%(month)s", "Day": "%(day)s", "Hour": "%(hour)s:%(minute)s:%(second)s" }""" % { "year": today.year, "month": today.month, "day": today.day, "hour": today.hour, "minute": today.minute, "second": today.second } 
 
  security = ""
 
  if(keyLength == 0):
   print("0 yazılamaz")
  
- if(keyLength <= 4):
+ if(keyLength < 8):
   security += "Guvenli Degil!"
 
  elif(keyLength >= 8):
   security += "Guvenli!"
+
 
 # FONKSİYON / 1
  def addToCreated(dataLength):
@@ -67,18 +91,21 @@ else:
 
 };
 
- """ % { "sts": security, "nm": userName, "trh": time, "gen": passw + i})
+module.exports = i; """ % { "sts": security, "nm": userName, "trh": time, "gen": passw + i})
 
   file2.write(
  """
-Password: '%(gen)s'
-Name: '%(nm)s'
-Durum: '%(sts)s'
-Tarih: %(trh)s
+|-----------------------|
+| Password: '%(gen)s'
+| Name: '%(nm)s'
+| Durum: '%(sts)s'
+| Tarih: %(trh)s
+|-----------------------|
  """ % { "sts": security, "nm": userName, "trh": time, "gen": passw + i})
   print("Şifreniz: %(gen)s \n\nSistem: Şifreniz '%(name)s' ve '%(name2)s' dosyasına kaydedildi!" % { "name2": fileName2, "name": fileName, "gen": data})
   file.close()
   file2.close()
+
 
 # FONKSİYON / 2
  def createPlainPassword(dataLength):
@@ -100,25 +127,28 @@ Tarih: %(trh)s
 
 };
 
- """ % { "sts": security, "nm": userName, "trh": time, "gen": passw})
+module.exports = i;""" % { "sts": security, "nm": userName, "trh": time, "gen": passw})
+
   file2.write(
  """
-Password: '%(gen)s'
-Name: '%(nm)s'
-Durum: '%(sts)s'
-Tarih: %(trh)s
-
+|---------------------|
+| Password: '%(gen)s'
+| Name: '%(nm)s'
+| Durum: '%(sts)s'
+| Tarih: %(trh)s
+|---------------------|
  """ % { "sts": security, "nm": userName, "trh": time, "gen": passw})
   print("Şifreniz: %(gen)s \n\nSistem: Şifreniz '%(name)s' ve '%(name2)s' dosyasına kaydedildi!" % { "name2": fileName2, "name": fileName, "gen": data})
   file.close()
  
+
  if(val == 1): 
-  print("Sistem uygulamayı başlatmayı deniyor...")
   addToCreated(keyLength);
 
+
  elif(val == 2):
-  print("Sistem uygulamayı başlatmayı deniyor...")
   createPlainPassword(keyLength);
+
 
  else:
   print("Geçersiz işlem!")
